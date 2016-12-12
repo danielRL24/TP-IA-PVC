6ae3e9a91644e62244e1acb5e66ec99075b2e224
Daniel Rodrigues Lourenço <daniel.24.rodrigues@gmail.com>
2016-12-12 10:03:07 +0100

    Aquisition des données

--- RuedinRodrigues.py
+++ RuedinRodrigues.py
@@ -41,48 +41,51 @@ def showGUI(cities):
     font = pygame.font.Font(None,30)
 
     def draw(positions):
-    		screen.fill(0)
-    		for pos in positions:
-    			pygame.draw.circle(screen,city_color,pos,city_radius)
-    		text = font.render("Nombre: %i" % len(positions), True, font_color)
-    		textRect = text.get_rect()
-    		screen.blit(text, textRect)
-    		pygame.display.flip()
+        screen.fill(0)
+        for pos in positions:
+            pygame.draw.circle(screen,city_color,(pos.x, pos.y),city_radius)
+            # pygame.draw.circle(screen,city_color,pos,city_radius)
+        text = font.render("Nombre: %i" % len(positions), True, font_color)
+        textRect = text.get_rect()
+        screen.blit(text, textRect)
+        pygame.display.flip()
 
     # cities = []
     draw(cities)
 
     collecting = True
 
-    citieName = "V"
     i = 0
     while collecting:
-    	for event in pygame.event.get():
-    		if event.type == QUIT:
-    			sys.exit(0)
-    		elif event.type == KEYDOWN and event.key == K_RETURN:
-    			collecting = False
-    		elif event.type == MOUSEBUTTONDOWN:
-    			cities.append(City(pygame.mouse.get_pos())
-    			draw(cities)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit(0)
+            elif event.type == KEYDOWN and event.key == K_RETURN:
+                collecting = False
+            elif event.type == MOUSEBUTTONDOWN:
+                cities.append(City("v"+str(i), pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]))
+                # cities.append(pygame.mouse.get_pos())
+                draw(cities)
+                i += 1
 
     screen.fill(0)
-    pygame.draw.lines(screen,city_color,True,cities)
+    # pygame.draw.lines(screen,city_color,True,cities)
     text = font.render("Un chemin, pas le meilleur!", True, font_color)
     textRect = text.get_rect()
     screen.blit(text, textRect)
     pygame.display.flip()
 
     while True:
-    	event = pygame.event.wait()
-    	if event.type == KEYDOWN: break
+        event = pygame.event.wait()
+        if event.type == KEYDOWN: break
 
 def readFile(filename, cities):
     ''' Load cities from file and put in a list '''
     try:
         with open (filename, 'r', encoding='utf-8') as input_file:
             for line in input_file:
-                cities.append(City(line.split[0], line.split[1], line.split[2]))
+                city = line.split(" ")
+                cities.append(City(city[0], city[1], city[2]))
     except FileNotFoundError:
         sys.exit("File positions.txt not found !")
 
@@ -92,6 +95,9 @@ def ga_solve(file=None, gui=True, maxtime=0):
         showGUI(cities)
     else:
         readFile(file, cities)
+
+    for city in cities:
+        print(city.name + "(" + str(city.x) +  ", " + str(city.y) + ")")
 
 if __name__ == '__main__':
     filename = None;
@@ -107,12 +113,12 @@ if __name__ == '__main__':
             print("Maxtime not a number ! (default value = 0)")
 
     for i in range(1, len(sys.argv)):
-        if sys.argv[i-1][0] != '-' and sys.argv[i][0] != '-':
+        if sys.argv[i][0] != '-' and sys.argv[i-1] != "--maxtime":
             filename = sys.argv[i]
 
 
     print(filename)
-    print(gui)
-    print(maxtime)
+    # print(gui)
+    # print(maxtime)
 
     ga_solve(filename, gui, maxtime)
